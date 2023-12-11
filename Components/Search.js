f664e91cc64bc538608034c8bd66783da6f9fd59
Fede C <federico.cipolletta@diestroweb.com>
2023-12-10 21:49:31 -0300

    Buscador con filtro de error por numero

--- Components/Search.js
+++ Components/Search.js
@@ -1,4 +1,4 @@
-import { StyleSheet, TextInput, Pressable, View } from 'react-native'
+import { StyleSheet, TextInput, Text, Pressable, View } from 'react-native'
 import { colors } from '../Global/colors'
 import { AntDesign, Entypo } from "@expo/vector-icons"
 import { useState } from 'react'
@@ -6,17 +6,34 @@ import { useState } from 'react'
 const Search = ({ setKeyword }) => {
 
     const [input, setInput] = useState("");
+    const [error, setError] = useState("");
 
+    const search = () => {
+        const regExHasNumber = /.*[0-9]*./;
+        if (regExHasNumber.test(input)) {
+            setError("Error! No debe contener numeros");
+
+        }
+        else
+            setKeyword(input);
+    }
+
+    const resetSearch = () => {
+        setInput("");
+        setError("");
+    }
     return (
-
         <View style={styles.container}>
-            <TextInput style={styles.input} placeholder='Buscar' value={input} onChangeText={(t) => setInput(t)} />
-            <Pressable onPress={() => setKeyword(input)}>
-                <AntDesign name='search1' color='black' size={25} />
-            </Pressable>
-            <Pressable onPress={() => setKeyword("")}>
-                <Entypo name='erase' color='black' size={25} />
-            </Pressable>
+            <View style={styles.searchContainer}>
+                <TextInput style={styles.input} placeholder='Buscar' value={input} onChangeText={(t) => setInput(t)} />
+                <Pressable onPress={() => { search() }}>
+                    <AntDesign name='search1' color='black' size={25} />
+                </Pressable>
+                <Pressable onPress={() => resetSearch()}>
+                    <Entypo name='erase' color='black' size={25} />
+                </Pressable>
+            </View>
+            {error ? <Text style={styles.errorStyle}>{error}</Text> : null}
         </View>
     )
 }
@@ -25,6 +42,10 @@ export default Search
 
 const styles = StyleSheet.create({
 
+    container: {
+        backgroundColor: colors.green1,
+        width: "100%",
+    },
     input: {
         borderWidth: 2,
         borderRadius: 5,
@@ -33,13 +54,15 @@ const styles = StyleSheet.create({
         backgroundColor: colors.green2,
 
     },
-    container: {
-        width: "100%",
+    searchContainer: {
         flexDirection: "row",
         alignItems: "center",
         padding: 10,
-        gap: 10,
-        backgroundColor: colors.green1
+        gap: 10
 
+    },
+    errorStyle: {
+        color: "red",
+        paddingHorizontal: 10
     }
 })
