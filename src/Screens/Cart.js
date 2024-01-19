ce54fff486bdae1c3a297ec5018ae82ac5cc14bb
Fede C <federico.cipolletta@diestroweb.com>
2024-01-18 23:44:26 -0300

    Luego de confirmar, muestro el modal y cuando da ok, borro el carrito

--- src/Screens/Cart.js
+++ src/Screens/Cart.js
@@ -1,4 +1,4 @@
-import { StyleSheet, FlatList, Pressable, View, Text } from 'react-native'
+import { StyleSheet, FlatList, Pressable, View, Text, Modal, Button } from 'react-native'
 import allCart from "../Data/cart.json"
 import { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
@@ -12,7 +12,22 @@ const Cart = () => {
     const dispatch = useDispatch();
     const cart = useSelector(state => state.cart.value);
     const [triggerPostOrder] = usePostOrdersMutation();
+    const [modalVisible, setModalVisible] = useState(false);
 
+    const handleOrderConfirmation = async () => {
+        try {
+            console.log("running")
+            await triggerPostOrder(cart);
+            setModalVisible(true);
+        } catch (error) {
+            console.error('Error posting order:', error);
+        }
+    };
+
+    const handleModalClose = () => {
+        setModalVisible(false);
+        dispatch(clearCart()); 
+    };
 
     return (
         <View style={styles.container}>
@@ -23,14 +38,21 @@ const Cart = () => {
             />
             <View style={styles.totalContainer}>
                 <Text style={styles.text}>Total: ${cart.total}</Text>
-                <Pressable onPress={() => {
-                    triggerPostOrder(cart); 
-                    dispatch(clearCart());
-                    /*Show modal*/
-                }}>
+                <Pressable onPress={() => handleOrderConfirmation()}>
                     <Text style={styles.text}>Confirmar</Text>
                 </Pressable>
             </View>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={handleModalClose}
+            >
+                <View style={styles.modalContainer}>
+                    <Text>Order Successful!</Text>
+                    <Button title="OK" onPress={handleModalClose} />
+                </View>
+            </Modal>
         </View>
 
     )
@@ -56,6 +78,10 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderRadius: 20,
         padding: 10,
-
-    }
+    },
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 })
