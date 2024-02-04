3734ef2e290e43ffd936b4e675596edebaca6ed9
Fede C <federico.cipolletta@diestroweb.com>
2024-02-04 19:19:29 -0300

    Fix en ventana de categorias

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,42 +1,27 @@
-import React from 'react';
-import { FlatList, StyleSheet, TouchableOpacity, Text } from 'react-native';
-import CategoryItem from './CategoryItem';
-import { useGetCategoriesQuery } from '../app/sevices/shopServices';
+import { FlatList, StyleSheet} from 'react-native'
+
+import CategoryItem from './CategoryItem'
+import { useGetCategoriesQuery } from '../app/sevices/shopServices'
 import { colors } from '../Global/colors';
 
 const Categories = ({ navigation, route }) => {
-  const { data: categories, isLoading, error } = useGetCategoriesQuery();
+    const {data:categories, isLoading, error}=useGetCategoriesQuery();
+    return (
+        <FlatList
+            style={styles.container}
+            data={categories}
+            keyExtractor={item => item}
+            renderItem={({ item }) => <CategoryItem category={item} navigation={navigation} route={route}/>}
+        />
+    )
+}
 
-  return (
-    <FlatList
-      style={styles.container}
-      data={categories}
-      keyExtractor={(item) => item}
-      renderItem={({ item }) => (
-        <TouchableOpacity
-          style={styles.categoryButton}
-          onPress={() =>
-            navigation.navigate('ItemListCategories', { category: item })
-          }>
-          <CategoryItem category={item} />
-        </TouchableOpacity>
-      )}
-    />
-  );
-};
-
-export default Categories;
+export default Categories
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: colors.secondaryColor2,
-  },
-  categoryButton: {
-    marginBottom: 15,
-    backgroundColor: colors.mainColor1,
-    borderRadius: 10,
-    padding: 15,
-  },
-});
+    container: {
+        width: "100%",
+        paddingTop:20,
+        backgroundColor:colors.secondaryColor2
+    }
+})
